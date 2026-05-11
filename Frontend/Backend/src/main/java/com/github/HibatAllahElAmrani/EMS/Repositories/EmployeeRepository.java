4c96272ad2dd5215b6a001db468e853bc3caf1a5
HibaCodes <hibatae0@gmail.com>
2026-05-11 09:34:51 +0100

    Remove duplicate folders

--- Frontend/Backend/src/main/java/com/github/HibatAllahElAmrani/EMS/Repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.HibatAllahElAmrani.EMS.Repositories;
-
-import com.github.HibatAllahElAmrani.EMS.Entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
